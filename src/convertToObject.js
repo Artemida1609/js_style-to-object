9682459f66cdc2ee65644943e20dac76a592c6c1
Artem <skickoartem74@gmail.com>
2024-10-27 16:11:30 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,11 +10,11 @@ function convertToObject(sourceString) {
   const result = {};
 
   for (const item of items) {
-    const params = item.split(':');
+    const keyValue = item.split(':');
 
-    if (params.length === 2) {
-      const key = params[0].trim();
-      const value = params[1].trim();
+    if (keyValue.length === 2) {
+      const key = keyValue[0].trim();
+      const value = keyValue[1].trim();
 
       result[key] = value;
     }
